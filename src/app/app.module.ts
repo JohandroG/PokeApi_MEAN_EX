a4b96443b4e5283a41129b2a41a358e1236057a0
JohandroG <johandrogonzalez@gmail.com>
2021-11-30 18:14:30 -0600

    PokeApi_MEAN_EX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokeComponent } from './poke/poke.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokeComponent
   ],
   imports: [
     BrowserModule,
